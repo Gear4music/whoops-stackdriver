6a15ee0a967de2a27bb438ea942d381fff2cf945
Dan Cryer <dan@dancryer.com>
2020-06-08 14:10:47 +0100

    Update ExceptionWithContext.php

--- src/Exceptions/ExceptionWithContext.php
+++ src/Exceptions/ExceptionWithContext.php
@@ -6,12 +6,6 @@ use Throwable;
 
 interface ExceptionWithContext
 {
-    /** @return string */
-    public function getServiceName() : string;
-
-    /** @return string */
-    public function getServiceVersion() : string;
-
     /** @return array */
     public function getContext() : array;
 
